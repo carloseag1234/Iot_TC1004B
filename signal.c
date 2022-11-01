966d9a78144576ea25bcb0493e3a93180ae1bf65
carloseag1234 <carloseag1234@gmail.com>
2022-11-01 13:32:41 +0000

    A

--- signal.c
+++ signal.c
@@ -1,8 +1,25 @@
-#include <studio.h>
+#include <stdio.h>
 #include <signal.h>
 #include <unistd.h>
-
-int con;
+int a = 1;
 void holaMundo(int signalNumber){
-    if 
+    if(signalNumber == 10){
+        printf("Es la senal 10\n");
+    } else {
+        printf("Es otra senal");
+    }
+    a = 2; 
+}
+void noMeMatas(int sig){
+    printf("\nXDDDD, te la comes perro\n");
+}
+int main(){
+    signal(10,holaMundo);
+
+    while(a==1){
+        printf("Estoy trabajando\n");
+        sleep(1);
+    }
+    printf("Nunca llega\n");
+    return 0;
 }
